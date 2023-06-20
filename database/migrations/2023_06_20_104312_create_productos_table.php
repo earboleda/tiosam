c7849b63cfaa2084711f814b9fed031f4f0f48b4
Euclides Arboleda <euclides.arboleda@gmail.com>
2023-06-20 12:34:26 -0500

    Agregar administrador de productos

---
+++ database/migrations/2023_06_20_104312_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion',100);
+            $table->integer('id_unidad');
+            $table->integer('id_tipoproducto');
+            $table->decimal('precio',10,2);
+            $table->integer('id_estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
